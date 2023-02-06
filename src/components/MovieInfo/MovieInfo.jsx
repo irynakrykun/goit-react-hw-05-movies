279c292a158610192d0ff331fa9e699575b7d8a6
irynakrykun <ira_franyk@ukr.net>
2023-02-06 15:59:45 +0200

    styled

--- src/components/MovieInfo/MovieInfo.jsx
+++ src/components/MovieInfo/MovieInfo.jsx
@@ -1,18 +1,27 @@
+
+import { ImgBlock,ContainerInfo,CardContainer } from "./MovieInfo.styled";
+
+
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 
 export const MovieInfo = ({
-  movies: { title, poster_path, overview, genres, name },
+  movies: { title, poster_path, overview, genres, name,vote_average,release_date },
 }) => {
   return (
-    <div>
-      <h2>{title || name}</h2>
-      <img src={IMG_URL + poster_path} alt={title} width="180" />
+    <>
+    <CardContainer>
+      <ImgBlock src={IMG_URL + poster_path} alt={title} width="180" />
+      <ContainerInfo>
+        <h2>{title || name} {`${new Date(release_date).getFullYear()}`}</h2>
+        <p>User score: {Math.ceil(vote_average * 10)} %</p>
       <h2>Overview</h2>
       <p>{overview}</p>
       <h2>Gernes</h2>
       <p>
         {genres?.length > 0 ? genres.map(genre => genre.name).join(', ') : '-'}
       </p>
-    </div>
+      </ContainerInfo>
+    </CardContainer>
+    </>
   );
 };
---
+++ src/components/MovieInfo/MovieInfo.styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+export const ContainerInfo = styled.div`
+  display:block;
+  margin-right: 50%
+  margin-left:50px
+  `;
+export const ImgBlock = styled.img`
+      display: block;
+    max-width: 100%;
+    margin-right: 30px;
+    margin-top: 10px`
+export const CardContainer = styled.div`
+    display:flex ;`
